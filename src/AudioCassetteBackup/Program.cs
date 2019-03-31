4af3f98c247b07988c9add64936bec61897d1974
Dmitry Pavlov <me@dmitry-pavlov.com>
2019-03-31 22:10:36 +0300

    Added initial project for https://github.com/dmitry-pavlov/audio-cassette-backup

---
+++ src/AudioCassetteBackup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AudioCassetteBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I wish I had a tape recorder on April fool’s day. :)");
+        }
+    }
+}
